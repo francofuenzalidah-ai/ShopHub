bdf9f6ab68cb630aafd3e425680111e75793fb34
Franco Fuenzalida <ffuenzalida@cmetrix.la>
2026-05-13 16:45:17 -0400

    added auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,22 @@ import AuthPage from './pages/AuthPage'
 import HomePage from './pages/HomePage'
 import CheckoutPage from './pages/CheckoutPage'
 import Navbar from './components/Navbar'
+import AuthProvider from './context/AuthContext'
 import './App.css'
 
 function App() {
 
   return (
-    <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/checkout" element={<CheckoutPage />} />
-      </Routes>
-    </div>
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
+        </Routes>
+      </div>
+    </AuthProvider>
   )
 }
 
